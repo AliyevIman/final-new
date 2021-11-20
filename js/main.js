c75138496909001d0093a7ad89c387c2afd5aa2c
iman111 <imaneliyev315@gmail.com>
2021-11-20 17:37:15 +0400

    22.22

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-var ad="meherrem"
-var soyad="exizov"
-console.error(ad.toLowerCase())
-console.error(ad.toLocaleUpperCase())
+"use strict"
+
+
+
+// $(".accordion .accordion-item button").click(function(){
+
+
+
+
+
+
+//     $(this).next().stop().toggle(500);
+
+
+
+
+
+
+
+// })
+
+var btn = document.querySelectorAll(".accordion button")
+var p = document.querySelectorAll(".accordion ul")
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].onclick = function () {
+        // for (let j = 0; j < p.length; j++) {
+        //     p[j].classList.remove("active")
+        // }
+        this.nextElementSibling.classList.toggle("active")
+    }
+
+}
